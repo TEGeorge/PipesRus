76d16da6de230d7f028c30a1b883198f1ea183f0
up719079 <up719079@myport.ac.uk>
2015-11-26 08:52:17 +0000

    -commenting complete

--- PipesRus/src/PipeV.java
+++ PipesRus/src/PipeV.java
@@ -1,8 +1,23 @@
+/**
+ * Implementation of abstract class Pipe
+ * @author UP719079 & UP
+ */
 import java.util.ArrayList;
 
 public class PipeV extends Pipe
 {
 
+  /**
+   * Executes Pipe super constructor
+   * This calls the valid method which can throw an exception
+   * @param   int plastic grade
+   * @param   int number of colours
+   * @param   boolean inner insulated
+   * @param   boolean reinforced
+   * @param   boolean chemical resistant
+   * @param   double pipe length
+   * @param   double outer diameter
+   */
   public PipeV(int plastic,int colours, Boolean insulated,
                     Boolean reinforced, Boolean chemicalResist,
                     double length,double outerDiameter)
@@ -11,6 +26,10 @@ public class PipeV extends Pipe
     valid();
   }
 
+  /**
+   * checks that all constructor parameters are legal
+   * if not an exception is thrown that must be caught when creating the object
+   */
   protected void valid() throws IllegalArgumentException{
     if (!insulated || !reinforced || plastic > 2 || plastic < 6 || colours == 2)
     {
